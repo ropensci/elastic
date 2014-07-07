24da8a48424693d97e1252ef3ce3de4157d69525
Scott Chamberlain <myrmecocystus@gmail.com>
2014-07-06 22:35:50 -0700

    fixes es_get to include index in search url, closes #7

--- R/es_get.r
+++ R/es_get.r
@@ -7,19 +7,20 @@
 #' @template get
 #' @param exists XXX
 #' @details There are a lot of terms you can use for Elasticsearch. See here 
-#'    \url{http://www.elasticsearch.org/guide/reference/query-dsl/} for the documentation.
+#'    \url{http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/docs-get.html} for 
+#'    the documentation.
 #' @export
 #' @examples \dontrun{
 #' es_get(index='twitter', type='tweet', id=1)
+#' es_get(index='mran', type='metadata', id='taxize')
+#' es_get(index='mran', type='metadata', id='taxize', source=TRUE)
 #' 
 #' # Get certain fields
 #' es_get(index='twitter', type='tweet', id=1, fields='user')
 #' 
 #' # Just test for existence of the document
 #' es_get(index='twitter', type='tweet', id=1, exists=TRUE)
-#' 
-#' # Just get source (NOT WORKING RIGHT NOW)
-#' es_get(index='twitter', type='tweet', id=1, source=TRUE)
+#' es_get(index='mran', type='metadata', id='taxize', exists=TRUE)
 #' }
 
 es_get <- function(index=NULL, type=NULL, id=NULL, source=FALSE, 
@@ -30,12 +31,13 @@ es_get <- function(index=NULL, type=NULL, id=NULL, source=FALSE,
   if(!is.null(fields)) fields <- paste(fields, collapse=",")
   
   url <- paste(conn$base, ":", conn$port, sep="")
+  args <- es_compact(list(fields = fields, ...))
+  url <- sprintf("%s/%s/%s/%s", url, index, type, id) 
   if(source) url <- paste(url, '_source', sep="/")
-  args <- es_compact(list(fields = fields, id=id, ...))
   
-  if(length(id) > 1){ # pass in request in body
-    body <- toJSON(list(ids = as.character(id)))
-  } else{ url <- sprintf("%s/%s/%s", url, type, id) }
+#   if(length(id) > 1){ # pass in request in body
+#     body <- toJSON(list(ids = as.character(id)))
+#   } else{ url <- sprintf("%s/%s/%s/%s", url, index, type, id) }
   
   if(exists){
     out <- HEAD(url, query=args, callopts)
