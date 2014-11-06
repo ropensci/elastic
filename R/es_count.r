adc09d76a0961b8bd67fefc9ffa1a9757190a434
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-06 07:09:21 -0800

    updated es_count man file

--- R/es_count.r
+++ R/es_count.r
@@ -9,13 +9,16 @@
 #' @param ... Further args passed on to elastic search HTTP API as parameters.
 #'
 #' @details There are a lot of terms you can use for Elasticsearch. See here
-#'    \url{http://www.elasticsearch.org/guide/reference/query-dsl/} for the documentation.
-#'
+#' \url{http://www.elasticsearch.org/guide/reference/query-dsl/} for the documentation.
+#' See docs for the count API here 
+#' \url{http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/search-count.html}
 #' @examples \dontrun{
 #' es_count()
-#' es_count(index='mran')
-#' es_count(index='mran', type='metadata')
-#' es_count(index='twitter')
+#' es_count(index='plos')
+#' es_count(index='plos', type='article')
+#' es_count(index='shakespeare')
+#' es_count(index='plos,shakespeare', q="a*")
+#' es_count(index='plos,shakespeare', q="z*")
 #' }
 
 es_count <- function(index=NULL, type=NULL, callopts=list(), verbose=TRUE, ...)
