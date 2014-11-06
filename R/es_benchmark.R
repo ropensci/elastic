5ce43c604243d2bea23bb05326aafc066f278e31
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-05 23:56:45 -0800

    started benchmark fxn, #17 but not exporting, as user has to turn on es with flag command to turn this on

---
+++ R/es_benchmark.R
@@ -0,0 +1,67 @@
+#' Run benchmark tests
+#'
+#' @param index Index name
+#' @param type Document type
+#' @param raw If TRUE (default), data is parsed to list. If FALSE, then raw JSON.
+#' @param callopts Curl args passed on to httr::POST.
+#' @param query Query, either a list or json.
+#' @param ... Further args passed on to elastic search HTTP API as parameters. Not used right now.
+#' @examples \donttest{ 
+#' # pass in as an R list
+#' args <- list(name = "b1", competitors = list(name = "c1", requests = list(query = list(match = "a*"))))
+#' es_benchmark(index="shakespeare", query=aggs)
+#' 
+#' # or pass in as json query with newlines, easy to read
+#' aggs <- '{
+#' "name": "b1",
+#' "competitors": [ {
+#' "name": "c1",
+#'   "requests": [ {
+#'     "query": {
+#'       "match": { "_all": "a*" }
+#'     }
+#'   } ]
+#' } ]
+#' }'
+#' es_benchmark(query=aggs)
+#' 
+#' # or pass in collapsed json string
+#' aggs <- '{"aggs":{"stats":{"terms":{"field":"text_entry"}}}}'
+#' es_search_body(index="shakespeare", query=aggs)
+#' 
+#' # match query
+#' match <- '{"query": {"match" : {"text_entry" : "Two Gentlemen"}}}'
+#' es_search_body(index="shakespeare", query=match)
+#' 
+#' # multi-match (multiple fields that is) query
+#' mmatch <- '{"query": {"multi_match" : {"query" : "henry", "fields": ["text_entry","play_name"]}}}'
+#' es_search_body(index="shakespeare", query=mmatch)
+#' 
+#' # bool query
+#' mmatch <- '{
+#'  "query": {
+#'    "bool" : {
+#'      "must_not" : {
+#'        "range" : {
+#'          "speech_number" : {
+#'            "from" : 1, "to": 5 
+#' }}}}}}'
+#' }
+
+es_benchmark <- function(raw=FALSE, callopts=list(), query=list(), ...)
+{
+  bench_POST(query = query, raw = raw, callopts = callopts, ...)
+}
+
+bench_POST <- function(query, raw, callopts, ...){
+  conn <- es_get_auth()
+  url <- paste0(conn$base, ":", conn$port, "/shakespeare/_bench")
+  args <- check_inputs(query)
+  tt <- PUT(url, body=args, callopts, encode = "json")
+  if(tt$status_code > 202){
+    if(tt$status_code > 202) stop(tt$headers$statusmessage)
+    if(content(tt)$status == "ERROR") stop(content(tt)$error_message)
+  }
+  res <- content(tt, as = "text")
+  if(raw) res else jsonlite::fromJSON(res, FALSE)
+}
