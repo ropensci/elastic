fe17840bc6da26f946bc79d6f579e56b83181b88
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-07 18:08:51 -0800

    ignoring internal fxns that prepare bulk data for now while they require addtn pkg

--- R/es_bulk.r
+++ R/es_bulk.r
@@ -44,29 +44,29 @@ es_bulk <- function(filename, raw=FALSE, callopts=list())
 #   message(sprintf("File written to %s", filename))
 # }
 # 
-proc_doc <- function(x){
-  b <- jsonlite::toJSON(x, auto_unbox = TRUE)
-  gsub("\\[|\\]", "", as.character(b))
-}
-
-# make_bulk_gbif(900, filename="inst/examples/gbif_data.json")
-make_bulk_gbif <- function(n = 600, filename = "~/gbif_data.json"){  
-  unlink(filename)
-  res <- lapply(seq(1, n, 300), getgbif)
-  res <- do.call(c, res)
-  res <- lapply(res, function(x){
-    x[sapply(x, length)==0] <- "null"
-    lapply(x, function(y) if(length(y) > 1) paste0(y, collapse = ",") else y)
-  })
-  for(i in seq_along(res)){
-    dat <- list(index = list(`_index` = "gbif", `_type` = "record", `_id` = i-1))
-    cat(proc_doc(dat), sep = "\n", file = filename, append = TRUE)
-    cat(proc_doc(res[[i]]), sep = "\n", file = filename, append = TRUE)
-  }  
-  message(sprintf("File written to %s", filename))
-}
-
-getgbif <- function(x){
-  res <- GET("http://api.gbif.org/v1/occurrence/search", query=list(limit=300, offset=x))
-  jsonlite::fromJSON(content(res, "text"), FALSE)$results
-}
+# proc_doc <- function(x){
+#   b <- jsonlite::toJSON(x, auto_unbox = TRUE)
+#   gsub("\\[|\\]", "", as.character(b))
+# }
+# 
+# # make_bulk_gbif(900, filename="inst/examples/gbif_data.json")
+# make_bulk_gbif <- function(n = 600, filename = "~/gbif_data.json"){  
+#   unlink(filename)
+#   res <- lapply(seq(1, n, 300), getgbif)
+#   res <- do.call(c, res)
+#   res <- lapply(res, function(x){
+#     x[sapply(x, length)==0] <- "null"
+#     lapply(x, function(y) if(length(y) > 1) paste0(y, collapse = ",") else y)
+#   })
+#   for(i in seq_along(res)){
+#     dat <- list(index = list(`_index` = "gbif", `_type` = "record", `_id` = i-1))
+#     cat(proc_doc(dat), sep = "\n", file = filename, append = TRUE)
+#     cat(proc_doc(res[[i]]), sep = "\n", file = filename, append = TRUE)
+#   }  
+#   message(sprintf("File written to %s", filename))
+# }
+# 
+# getgbif <- function(x){
+#   res <- GET("http://api.gbif.org/v1/occurrence/search", query=list(limit=300, offset=x))
+#   jsonlite::fromJSON(content(res, "text"), FALSE)$results
+# }
