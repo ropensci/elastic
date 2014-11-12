61cef1787545bc9f68e86cbc48cf33af2c2443aa
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-11 22:19:06 -0800

    renamed es_count to count to remove prefix

--- R/es_count.r
+++ R/es_count.r
@@ -1,28 +1,23 @@
 #' Get counts of the number of records per index.
 #'
 #' @export
-#'
 #' @param index Index, defaults to all indices
 #' @param type Document type
 #' @param callopts Curl args passed on to httr::GET.
 #' @param verbose If TRUE (default) the url call used printed to console.
 #' @param ... Further args passed on to elastic search HTTP API as parameters.
-#'
-#' @details There are a lot of terms you can use for Elasticsearch. See here
-#' \url{http://www.elasticsearch.org/guide/reference/query-dsl/} for the documentation.
-#' See docs for the count API here 
+#' @details See docs for the count API here 
 #' \url{http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/search-count.html}
-#' @examples \dontrun{
-#' es_count()
-#' es_count(index='plos')
-#' es_count(index='plos', type='article')
-#' es_count(index='shakespeare')
-#' es_count(index='plos,shakespeare', q="a*")
-#' es_count(index='plos,shakespeare', q="z*")
+#' @examples \donttest{
+#' count()
+#' count(index='plos')
+#' count(index='plos', type='article')
+#' count(index='shakespeare')
+#' count(index=c('plos','shakespeare'), q="a*")
+#' count(index=c('plos','shakespeare'), q="z*")
 #' }
 
-es_count <- function(index=NULL, type=NULL, callopts=list(), verbose=TRUE, ...)
-{
-  out <- es_GET(path = '_count', index, type, NULL, NULL, NULL, FALSE, callopts, ...)
+count <- function(index=NULL, type=NULL, callopts=list(), verbose=TRUE, ...){
+  out <- es_GET(path = '_count', cl(index), type, NULL, NULL, NULL, FALSE, callopts, ...)
   jsonlite::fromJSON(out, FALSE)$count
 }
