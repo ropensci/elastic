b68eadbe3b3a1c7c540300012950e1a7ce9c30a8
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-11 23:02:51 -0800

    reworked cat fxns so that each method is a function instead of param renamed cat fxns, new cat level man file, #24

--- R/cat.r
+++ R/cat.r
@@ -1,9 +1,7 @@
 #' Use the cat Elasticsearch api.
 #' 
 #' @export
-#' 
-#' @param what What to print, one of '', aliases, allocation, count, segments, health, indices, 
-#' master, nodes, pending_tasks, plugins, recovery, thread_pool, or shards. 
+#' @name cat
 #' @param verbose If TRUE (default) the url call used printed to console.
 #' @param index Index name
 #' @param fields Fields to return, only used with \code{fielddata}
@@ -11,33 +9,97 @@
 #' @details See \url{http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/cat.html} 
 #' for the cat API documentation.
 #'
-#' @examples \dontrun{
-#' es_cat() # list Elasticsearch cat endpoints
-#' es_cat('aliases')
-#' es_cat('aliases', index='mran')
-#' es_cat('allocation')
-#' es_cat('allocation', verbose=TRUE)
-#' es_cat('count')
-#' es_cat('count', index='mran')
-#' es_cat('count', index='twitter')
-#' es_cat('segments')
-#' es_cat('segments', index='mran')
-#' es_cat('health')
-#' es_cat('indices')
-#' es_cat('master')
-#' es_cat('nodes')
-#' es_cat('pending_tasks')
-#' es_cat('plugins')
-#' es_cat('recovery', verbose=TRUE)
-#' es_cat('recovery', index='mran')
-#' es_cat('thread_pool')
-#' es_cat('thread_pool', verbose=TRUE)
-#' es_cat('shards')
-#' es_cat('fielddata')
-#' es_cat('fielddata', fields='body')
+#' @examples \donttest{
+#' # list Elasticsearch cat endpoints
+#' cat_() 
+#' 
+#' # Do other cat operations
+#' cat_aliases()
+#' cat_aliases(index='plos')
+#' cat_allocation()
+#' cat_allocation(verbose=TRUE)
+#' cat_count()
+#' cat_count(index='plos')
+#' cat_count(index='gbif')
+#' cat_segments()
+#' cat_segments(index='gbif')
+#' cat_health()
+#' cat_indices()
+#' cat_master()
+#' cat_nodes()
+#' cat_pending_tasks()
+#' cat_plugins()
+#' cat_recovery(verbose=TRUE)
+#' cat_recovery(index='gbif')
+#' cat_thread_pool()
+#' cat_thread_pool(verbose=TRUE)
+#' cat_shards()
+#' cat_fielddata()
+#' cat_fielddata(fields='body')
 #' }
 
-es_cat <- function(what='', verbose=FALSE, index=NULL, fields=NULL)
+#' @export
+#' @rdname cat
+cat_ <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('', v=verbose, i=index, f=fields)
+
+#' @export
+#' @rdname cat
+cat_aliases <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('aliases', v=verbose, i=index, f=fields)
+
+#' @export
+#' @rdname cat
+cat_allocation <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('allocation', v=verbose, i=index, f=fields)
+
+#' @export
+#' @rdname cat
+cat_count <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('count', v=verbose, i=index, f=fields)
+
+#' @export
+#' @rdname cat
+cat_segments <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('segments', v=verbose, i=index, f=fields)
+
+#' @export
+#' @rdname cat
+cat_health <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('health', v=verbose, i=index, f=fields)
+
+#' @export
+#' @rdname cat
+cat_indices <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('indices', v=verbose, i=index, f=fields)
+
+#' @export
+#' @rdname cat
+cat_master <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('master', v=verbose, i=index, f=fields)
+
+#' @export
+#' @rdname cat
+cat_nodes <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('nodes', v=verbose, i=index, f=fields)
+
+#' @export
+#' @rdname cat
+cat_pending_tasks <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('pending_tasks', v=verbose, i=index, f=fields)
+
+#' @export
+#' @rdname cat
+cat_plugins <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('plugins', v=verbose, i=index, f=fields)
+
+#' @export
+#' @rdname cat
+cat_recovery <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('recovery', v=verbose, i=index, f=fields)
+
+#' @export
+#' @rdname cat
+cat_thread_pool <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('thread_pool', v=verbose, i=index, f=fields)
+
+#' @export
+#' @rdname cat
+cat_shards <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('shards', v=verbose, i=index, f=fields)
+
+#' @export
+#' @rdname cat
+cat_fielddata <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('fielddata', v=verbose, i=index, f=fields)
+
+
+cat_helper <- function(what='', v=FALSE, i=NULL, f=NULL)
 {
   conn <- es_get_auth()
   if(!is.null(fields)) fields <- paste(fields, collapse=",")
@@ -48,5 +110,5 @@ es_cat <- function(what='', verbose=FALSE, index=NULL, fields=NULL)
   stop_for_status(out)
   if(verbose) message(URLdecode(out$url))
   dat <- content(out, as = "text")
-  if(identical(dat, "")) message("Nothing to print") else cat(dat)
+  if(identical(dat, "")) message("Nothing to print") else base::cat(dat)
 }
