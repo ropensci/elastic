2cb528ba6f64844bc59eb853cf6b8a63f227148d
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-12 14:42:13 -0800

    added search man file to list all search api fxns

---
+++ R/search.R
@@ -0,0 +1,17 @@
+#' Search functions.
+#' 
+#' @name search
+#' 
+#' @details  
+#' Elasticsearch search APIs include the following functions:
+#' \itemize{
+#'  \item \code{\link{es_search}} - Search using the URI search API
+#'  \item \code{\link{es_search_body}} - Pass in search specification in request body
+#'  \item \code{\link{mlt}} - More like this queries
+#'  \item \code{\link{count}} - Get counts for various searches
+#' }
+#' 
+#' More will be added soon.
+#' @references 
+#' \url{http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/search.html}
+NULL
