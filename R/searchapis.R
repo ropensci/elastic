74d8c464099e3ea39ba61907a934e29a2b85103a
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-12 16:13:35 -0800

    added new fxns to saerchapis manual page

--- R/searchapis.R
+++ R/searchapis.R
@@ -6,8 +6,12 @@
 #' \itemize{
 #'  \item \code{\link{es_search}} - Search using the URI search API
 #'  \item \code{\link{search_body}} - Pass in search specification in request body
+#'  \item \code{\link{search_shards}} - Search shards
 #'  \item \code{\link{mlt}} - More like this queries
 #'  \item \code{\link{count}} - Get counts for various searches
+#'  \item \code{\link{explain}} - Computes a score explanation for a query and a specific 
+#'  document. This can give useful feedback whether a document matches or didn’t match 
+#'  a specific query.
 #' }
 #' 
 #' More will be added soon.
