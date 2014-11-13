13ffb5868908fccdbeac813aad10cdd5ae09d87c
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-12 16:13:16 -0800

    fixed docs_get fxn

--- R/docs_get.r
+++ R/docs_get.r
@@ -1,36 +1,32 @@
 #' Get documents via the get API.
 #'
-#' @import httr 
-#'
+#' @export
 #' @template all
 #' @template get
-#' @param exists XXX
-#' @details There are a lot of terms you can use for Elasticsearch. See here
-#'    \url{http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/docs-get.html} for
-#'    the documentation.
-#' @export
+#' @param exists (logical) Only return a logical as to whether the document exists or not.
+#' @references 
+#' \url{http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/docs-get.html}
 #' @examples \dontrun{
-#' docs_get(index='twitter', type='tweet', id=1)
-#' docs_get(index='mran', type='metadata', id='taxize')
-#' docs_get(index='mran', type='metadata', id='taxize', source=TRUE)
+#' docs_get(index='shakespeare', type='line', id=10)
+#' docs_get(index='shakespeare', type='line', id=3)
+#' docs_get(index='shakespeare', type='line', id=3, source=TRUE)
 #'
 #' # Get certain fields
-#' docs_get(index='twitter', type='tweet', id=1, fields='user')
+#' docs_get(index='shakespeare', type='line', id=10, fields='play_name')
 #'
 #' # Just test for existence of the document
-#' docs_get(index='twitter', type='tweet', id=1, exists=TRUE)
-#' docs_get(index='mran', type='metadata', id='taxize', exists=TRUE)
+#' docs_get(index='plos', type='article', id=1, exists=TRUE)
+#' docs_get(index='plos', type='article', id=123456, exists=TRUE)
 #' }
 
 docs_get <- function(index=NULL, type=NULL, id=NULL, source=FALSE,
   fields=NULL, exists=FALSE, raw=FALSE, callopts=list(), verbose=TRUE, ...)
 {
   conn <- es_get_auth()
-
   if(!is.null(fields)) fields <- paste(fields, collapse=",")
 
   url <- paste(conn$base, ":", conn$port, sep="")
-  args <- ec(list(fields = fields, ...))
+  args <- ec(list(fields = cl(fields), ...))
   url <- sprintf("%s/%s/%s/%s", url, index, type, id)
   if(source) url <- paste(url, '_source', sep="/")
 
@@ -40,14 +36,12 @@ docs_get <- function(index=NULL, type=NULL, id=NULL, source=FALSE,
 
   if(exists){
     out <- HEAD(url, query=args, callopts)
-    message(paste(out$headers[c('status','statusmessage')], collapse=" - "))
+    if(out$status_code == 200) TRUE else FALSE
   } else
   {
     out <- GET(url, query=args, callopts)
     stop_for_status(out)
     if(verbose) message(URLdecode(out$url))
-    tt <- content(out, as="text")
-    class(tt) <- "es_GET"
-    if(raw){ tt } else { es_parse(tt) }
+    if(raw){ content(out, as="text") } else { jsonlite::fromJSON(content(out, as="text"), FALSE) }
   }
 }
