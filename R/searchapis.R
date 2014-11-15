b5ca6b1d99bd760b973dd1165aefb298f4837711
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-15 07:49:50 -0800

    updated searchapis man page with es_search fxn name change

--- R/searchapis.R
+++ R/searchapis.R
@@ -1,15 +1,14 @@
 #' Search functions.
 #' 
 #' @name searchapis
-#' @details  
+#' @details
 #' Elasticsearch search APIs include the following functions:
 #' \itemize{
-#'  \item \code{\link{es_search}} - Search using the URI search API
-#'  \item \code{\link{search_body}} - Pass in search specification in request body 
-#'  DEPRECATED, use \code{\link{es_search}}
-#'  \item \code{\link{search_shards}} - Search shards
-#'  \item \code{\link{mlt}} - More like this queries
-#'  \item \code{\link{count}} - Get counts for various searches
+#'  \item \code{\link{search}} - Search using the URI search API or pass in query 
+#'  specification in the body of the request.
+#'  \item \code{\link{search_shards}} - Search shards.
+#'  \item \code{\link{mlt}} - More like this queries.
+#'  \item \code{\link{count}} - Get counts for various searches.
 #'  \item \code{\link{explain}} - Computes a score explanation for a query and a specific 
 #'  document. This can give useful feedback whether a document matches or didn't match 
 #'  a specific query.
