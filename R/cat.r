3f097fbcf2c0884a04e94426cabdf312cd998ef3
Scott Chamberlain <myrmecocystus@gmail.com>
2015-01-28 10:07:34 -0800

    added ... to all cat fxns to pass in curl options

--- R/cat.r
+++ R/cat.r
@@ -1,18 +1,21 @@
 #' Use the cat Elasticsearch api.
-#' 
-#' @export
+#'
 #' @name cat
 #' @param verbose If TRUE (default) the url call used printed to console.
 #' @param index Index name
 #' @param fields Fields to return, only used with \code{fielddata}
-#' 
-#' @details See \url{http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/cat.html} 
+#' @param ... Curl args passed on to \code{\link[httr]{POST}}
+#'
+#' @details See \url{http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/cat.html}
 #' for the cat API documentation.
+#' 
+#' Note how \code{\link{cat_}} has an underscore at the end to avoid conflict with the function 
+#' \code{\link{cat}} in base R.
 #'
 #' @examples \donttest{
 #' # list Elasticsearch cat endpoints
-#' cat_() 
-#' 
+#' cat_()
+#'
 #' # Do other cat operations
 #' cat_aliases()
 #' cat_aliases(index='plos')
@@ -36,77 +39,81 @@
 #' cat_shards()
 #' cat_fielddata()
 #' cat_fielddata(fields='body')
+#' 
+#' # Curl options
+#' library("httr")
+#' cat_count(config=verbose()) 
 #' }
 
 #' @export
 #' @rdname cat
-cat_ <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('', v=verbose, i=index, f=fields)
+cat_ <- function(verbose=FALSE, index=NULL, fields=NULL, ...) cat_helper('', v=verbose, i=index, f=fields, ...)
 
 #' @export
 #' @rdname cat
-cat_aliases <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('aliases', v=verbose, i=index, f=fields)
+cat_aliases <- function(verbose=FALSE, index=NULL, fields=NULL, ...) cat_helper('aliases', v=verbose, i=index, f=fields, ...)
 
 #' @export
 #' @rdname cat
-cat_allocation <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('allocation', v=verbose, i=index, f=fields)
+cat_allocation <- function(verbose=FALSE, index=NULL, fields=NULL, ...) cat_helper('allocation', v=verbose, i=index, f=fields, ...)
 
 #' @export
 #' @rdname cat
-cat_count <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('count', v=verbose, i=index, f=fields)
+cat_count <- function(verbose=FALSE, index=NULL, fields=NULL, ...) cat_helper('count', v=verbose, i=index, f=fields, ...)
 
 #' @export
 #' @rdname cat
-cat_segments <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('segments', v=verbose, i=index, f=fields)
+cat_segments <- function(verbose=FALSE, index=NULL, fields=NULL, ...) cat_helper('segments', v=verbose, i=index, f=fields, ...)
 
 #' @export
 #' @rdname cat
-cat_health <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('health', v=verbose, i=index, f=fields)
+cat_health <- function(verbose=FALSE, index=NULL, fields=NULL, ...) cat_helper('health', v=verbose, i=index, f=fields, ...)
 
 #' @export
 #' @rdname cat
-cat_indices <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('indices', v=verbose, i=index, f=fields)
+cat_indices <- function(verbose=FALSE, index=NULL, fields=NULL, ...) cat_helper('indices', v=verbose, i=index, f=fields, ...)
 
 #' @export
 #' @rdname cat
-cat_master <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('master', v=verbose, i=index, f=fields)
+cat_master <- function(verbose=FALSE, index=NULL, fields=NULL, ...) cat_helper('master', v=verbose, i=index, f=fields, ...)
 
 #' @export
 #' @rdname cat
-cat_nodes <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('nodes', v=verbose, i=index, f=fields)
+cat_nodes <- function(verbose=FALSE, index=NULL, fields=NULL, ...) cat_helper('nodes', v=verbose, i=index, f=fields, ...)
 
 #' @export
 #' @rdname cat
-cat_pending_tasks <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('pending_tasks', v=verbose, i=index, f=fields)
+cat_pending_tasks <- function(verbose=FALSE, index=NULL, fields=NULL, ...) cat_helper('pending_tasks', v=verbose, i=index, f=fields, ...)
 
 #' @export
 #' @rdname cat
-cat_plugins <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('plugins', v=verbose, i=index, f=fields)
+cat_plugins <- function(verbose=FALSE, index=NULL, fields=NULL, ...) cat_helper('plugins', v=verbose, i=index, f=fields, ...)
 
 #' @export
 #' @rdname cat
-cat_recovery <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('recovery', v=verbose, i=index, f=fields)
+cat_recovery <- function(verbose=FALSE, index=NULL, fields=NULL, ...) cat_helper('recovery', v=verbose, i=index, f=fields, ...)
 
 #' @export
 #' @rdname cat
-cat_thread_pool <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('thread_pool', v=verbose, i=index, f=fields)
+cat_thread_pool <- function(verbose=FALSE, index=NULL, fields=NULL, ...) cat_helper('thread_pool', v=verbose, i=index, f=fields, ...)
 
 #' @export
 #' @rdname cat
-cat_shards <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('shards', v=verbose, i=index, f=fields)
+cat_shards <- function(verbose=FALSE, index=NULL, fields=NULL, ...) cat_helper('shards', v=verbose, i=index, f=fields, ...)
 
 #' @export
 #' @rdname cat
-cat_fielddata <- function(verbose=FALSE, index=NULL, fields=NULL) cat_helper('fielddata', v=verbose, i=index, f=fields)
+cat_fielddata <- function(verbose=FALSE, index=NULL, fields=NULL, ...) cat_helper('fielddata', v=verbose, i=index, f=fields, ...)
 
 
-cat_helper <- function(what='', v=FALSE, i=NULL, f=NULL)
+cat_helper <- function(what='', v=FALSE, i=NULL, f=NULL, ...)
 {
   conn <- es_get_auth()
   if(!is.null(f)) f <- paste(f, collapse=",")
   url <- sprintf("%s:%s/_cat/%s", conn$base, conn$port, what)
-  if(!is.null(i)) url <- paste0(url, '/', i)  
+  if(!is.null(i)) url <- paste0(url, '/', i)
   args <- ec(list(v = if(v) '' else NULL, fields=f))
-  out <- GET(url, query=args)
+  out <- GET(url, query=args, ...)
   stop_for_status(out)
   if(v) message(URLdecode(out$url))
   dat <- content(out, as = "text")
