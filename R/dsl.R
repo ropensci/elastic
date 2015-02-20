8d02d6ce8a4a7d522421fde05702b7fb738a705f
Scott Chamberlain <myrmecocystus@gmail.com>
2015-02-20 11:34:59 -0800

    lots of changes in the dsl, #39

--- R/dsl.R
+++ R/dsl.R
@@ -26,21 +26,26 @@
 #' @examples \dontrun{
 #' # Get mapping for verifying fields requested in DSL
 #' index("shakespeare")
+#' index("shakespeare", "scene")
+#' index("shakespeare", "act")
 #' index("plos")
 #' index("gbif")
 #' 
-#' # Define bool query for the DSL
+#' # Define bool query
+#' bool(must_not = list(term=list(speaker="KING HENRY IV")))
 #' index("shakespeare") %>% 
-#'    bool(must = list(term=list(user="kimchy")))
-#' bool(must_not = list(term=list(user="kimchy")))
-#' bool(should = list(list(term=list(tag="sometag")), list(term=list(tag="someothertag"))))
+#'    bool(must = list(term=list(speaker="KING HENRY IV")))
+#' index("shakespeare") %>% 
+#'    bool(must_not = list(term=list(speaker="KING HENRY IV")))
+#' index("shakespeare") %>% 
+#'    bool(should = list(list(term=list(speech_number=1))))
 #' 
 #' bool(must = list(term=list(user="kimchy")), 
 #'      must_not = list(term=list(user="kimchy")))
 #'      
-#' # Define range query for the DSL
-#' range( speech_number == 5, line_id > 3 )
-#' range( speech_number <= c(1,5) )
+#' # Define range query
+#' index("shakespeare") %>% range( speech_number <= 5 )
+#' index("shakespeare") %>% range( speech_number <= c(1,5) )
 #' range( speech_number >= c(1,5) )
 #' }
 NULL
@@ -69,21 +74,81 @@ print.index <- function(x, ...){
 
 #' @export
 #' @rdname elasticdsl
-range <- function(.obj=list(), ..., boost=1, cache=FALSE) {
-  combine(.obj, .dots = lazyeval::lazy_dots(...))
+range <- function(.obj=list(), ..., boost=1, time_zone=NULL, execution=NULL, cache=FALSE) {
+  range_(.obj, .dots = lazyeval::lazy_dots(...))
+}
+
+#' @export
+#' @rdname elasticdsl
+range_ <- function(.obj=list(), ..., .dots) {
+  dots <- lazyeval::all_dots(.dots, ...)
+  query <- as.json(structure(dots, class=c("range","lazy_dots")))
+  execute(.obj, query)
 }
 
 #' @export
 #' @rdname elasticdsl
 bool <- function(.obj=list(), ...){
-  combine(.obj, .dots = lazyeval::lazy_dots(...))
-  # structure(unclass(tmp), class=c("equ","lazy_dots"))
+  bool_(.obj, .dots = lazyeval::lazy_dots(...))
+}
+
+#' @export
+#' @rdname elasticdsl
+bool_ <- function(.obj=list(), ..., .dots){
+  dots <- lazyeval::all_dots(.dots, ...)
+  query <- as.json(structure(dots, class=c("bool","lazy_dots")))
+  execute(.obj, query)
+}
+
+as.json <- function(x, ...) UseMethod("as.json")
+
+as.json.range <- function(x, ...){
+  x <- list(query = list(range = parse_range(get_eq(x[[1]]))))
+  jsonlite::toJSON(x, ..., auto_unbox = TRUE)
+}
+
+as.json.bool <- function(x, ...){
+  tmp <- setNames(list(lazy_eval(x[[1]]$expr)), names(x))
+  x <- list(query = list(bool = tmp))
+  jsonlite::toJSON(x, ..., auto_unbox = TRUE)
+}
+
+get_eq <- function(y) {
+  dat <- getParseData(parse(text = deparse(y$expr)))
+  tmp <- list(var = dat[ dat$token == "SYMBOL", "text"],
+              eq = dat$token[ dat$token %in% c("LT","GT","GE","LE","EQ_ASSIGN","EQ","NE") ],
+              num = dat[ dat$token == "NUM_CONST", "text"]
+  )
+  tmp$eq <- switch(tolower(tmp$eq), lt="lt", gt="gt", ge="gte", le="lte", eq_assign=NA, eq=NA)
+  tmp
+}
+
+parse_range <- function(x){
+  setNames(list(as.list(setNames(x$num, x$eq))), x$var)
 }
 
 # combine query statements
 combine <- function(.obj, ..., .dots){
-  c(.obj, lazyeval::all_dots(.dots, ...))
+  list(.obj, lazyeval::all_dots(.dots, ...))
 }
+
+# execute on Search
+execute <- function(.obj, query){
+  # query <- lazyeval::lazy_eval(query)
+  # query <- as.json(query)
+  Search_(index=attr(.obj, "index"), body=query)
+}
+
+# explain <- function(.obj=list(), ...){
+#   lazyeval::lazy_dots(...)
+#   # explain_(.obj, .dots = lazyeval::lazy_dots(...))
+# }
+# 
+# explain_ <- function(.obj=list(), ..., .dots){
+#   dots <- lazyeval::all_dots(.dots, ...)
+#   dots
+#   # structure(dots, class=c("explain","lazy_dots"))
+# }
 
 # shake <- get_map("shakespeare")
 # shake$line
@@ -101,33 +166,4 @@ get_map <- function(index, type=NULL, ...){
 # #' @export
 # print.equ <- function(x, pretty = TRUE, auto_unbox = TRUE, ...){
 #   print(jsonlite::toJSON(unclass(x), pretty=pretty, auto_unbox = auto_unbox))
-# }
-
-######
-# parse_input <- function(...){
-#   x <- as.character(dots(...))
-#   neg <- gsub('-', '', x[grepl("-", x)])
-#   pos <- x[!grepl("-", x, )]
-#   list(neg=neg, pos=pos)
-# }
-# 
-# dots <- function(...){
-#   eval(substitute(alist(...)))
-# }
-
-#####
-# select <- function (.data, ...) {
-#   select_(.data, .dots = lazyeval::lazy_dots(...))
-# }
-# 
-# select_.data.frame <- function (.data, ..., .dots) {
-#   lazyeval::all_dots(.dots, ...)
-# #   vars <- select_vars_(names(.data), dots)
-# #   select_impl(.data, vars)
-# }
-# 
-# lazy_dots <- function (..., .follow_symbols = FALSE) {
-#   if (nargs() == 0) 
-#     return(structure(list(), class = "lazy_dots"))
-#   .Call(make_lazy_dots, environment(), .follow_symbols)
 # }
