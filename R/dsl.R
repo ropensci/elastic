e165905585ee35e4b9f01fbebf3bf73f904beeea
Scott Chamberlain <myrmecocystus@gmail.com>
2015-02-19 16:21:44 -0800

    added single man page for the new DSL fxsn, #39

--- R/dsl.R
+++ R/dsl.R
@@ -1,58 +1,58 @@
-# stuff <- fromJSON('{
-#   "should" : [
-#     {
-#       "term" : { "tag" : "sometag" }
-#     },
-#     {
-#       "term" : { "tag" : "sometagtag" }
-#     }
-#   ]
-# }')
-# toJSON(stuff, pretty=TRUE)
-# 
-# q <- '{
-#   "query": {
-#     "bool" : {
-#       "must_not" : {
-#         "range" : {
-#           "speech_number" : {
-#             "from" : 1, "to": 5
-#           }
-#         }
-#       }
-#     }
-#   }
-# }'
-
-#' Get mapping for verifying fields requested in DSL
+#' The elastic DSL
 #' 
-#' @examples \dontrun{
-#' shake <- get_map("shakespeare")
-#' shake$line
-#' # field names
-#' names(shake$line$properties)
-#' # field types
-#' pluck(shake$line$properties, "type", "")
-#' shake$scene
-#' shake$act
+#' @import lazyeval
+#' @name elasticdsl
+#' 
+#' @param index asdfds
+#' @param type asdfds
+#' @param what asdfds
+#' @param .obj asdfds
+#' @param x adfadf
+#' @param ... adfadsf
+#' @details 
+#' The DSL for \code{elastic} makes it easy to do queries against an Elasticsearch 
+#' instance, either local or remote. 
+#' 
+#' The workflow with the DSL:
+#' \enumerate{
+#'  \item Start with the index to use, e.g., \code{index("shakespeare")}
+#'  \item Define queries, e.g., \code{range(speech_number == 5, line_id > 3)}
+#'  \item Execute search, e.g., \code{Search()}
 #' }
-get_map <- function(index, type=NULL, ...){
-  tmp <- mapping_get(index, type, ...)
-  tmp[[index]]$mappings
-}
-
-#' Get mapping for verifying fields requested in DSL
 #' 
+#' Alternatively, if nothing follows a query definition, \code{\link{Search}} is called
+#' to execute the search with the query as given. In a sense, this is essentially like 
+#' what \code{\link{dplyr}} does.
 #' @examples \dontrun{
+#' # Get mapping for verifying fields requested in DSL
 #' index("shakespeare")
 #' index("plos")
 #' index("gbif")
+#' 
+#' # Define bool query for the DSL
+#' index("shakespeare") %>% 
+#'    bool(must = list(term=list(user="kimchy")))
+#' bool(must_not = list(term=list(user="kimchy")))
+#' bool(should = list(list(term=list(tag="sometag")), list(term=list(tag="someothertag"))))
+#' 
+#' bool(must = list(term=list(user="kimchy")), 
+#'      must_not = list(term=list(user="kimchy")))
+#'      
+#' # Define range query for the DSL
+#' range( speech_number == 5, line_id > 3 )
+#' range( speech_number <= c(1,5) )
+#' range( speech_number >= c(1,5) )
 #' }
+NULL
+
+#' @export
+#' @rdname elasticdsl
 index <- function(index, type=NULL, what="mappings", ...){
   structure(get_map(index, type, ...), class="index", index=index, type=type)
 }
 
 #' @export
+#' @rdname elasticdsl
 print.index <- function(x, ...){
   cat("<index>", attr(x, "index"), "\n")
   cat("  type:", attr(x, "type"), "\n")
@@ -67,37 +67,35 @@ print.index <- function(x, ...){
   }
 }
 
-#' Define range query for the DSL
-#' 
-#' @import lazyeval
-#' @examples \dontrun{
-#' range( speech_number == 5, line_id > 3 )
-#' range( speech_number <= c(1,5) )
-#' range( speech_number >= c(1,5) )
-#' }
+#' @export
+#' @rdname elasticdsl
 range <- function(.obj=list(), ..., boost=1, cache=FALSE) {
   combine(.obj, .dots = lazyeval::lazy_dots(...))
 }
 
-#' Define bool query for the DSL
-#' 
-#' @examples \dontrun{
-#' index("shakespeare") %>% 
-#'    bool(must = list(term=list(user="kimchy")))
-#' bool(must_not = list(term=list(user="kimchy")))
-#' bool(should = list(list(term=list(tag="sometag")), list(term=list(tag="someothertag"))))
-#' 
-#' bool(must = list(term=list(user="kimchy")), 
-#'      must_not = list(term=list(user="kimchy")))
-#'      
-#' }
+#' @export
+#' @rdname elasticdsl
 bool <- function(.obj=list(), ...){
   combine(.obj, .dots = lazyeval::lazy_dots(...))
   # structure(unclass(tmp), class=c("equ","lazy_dots"))
 }
 
+# combine query statements
 combine <- function(.obj, ..., .dots){
   c(.obj, lazyeval::all_dots(.dots, ...))
+}
+
+# shake <- get_map("shakespeare")
+# shake$line
+# # field names
+# names(shake$line$properties)
+# # field types
+# pluck(shake$line$properties, "type", "")
+# shake$scene
+# shake$act
+get_map <- function(index, type=NULL, ...){
+  tmp <- mapping_get(index, type, ...)
+  tmp[[index]]$mappings
 }
 
 # #' @export
