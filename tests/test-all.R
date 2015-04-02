c835357f2829f9f234b5ddb782280cef6228e34e
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-02 13:13:31 -0700

    connect to host before putting docs in for tests

--- tests/test-all.R
+++ tests/test-all.R
@@ -2,6 +2,7 @@ library('testthat')
 library('elastic')
 
 shakespeare <- system.file("examples", "shakespeare_data.json", package = "elastic")
+invisible(elastic::connect())
 invisible(elastic::docs_bulk(shakespeare))
 
 test_check('elastic')
