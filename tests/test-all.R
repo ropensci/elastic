b5d8b2b7c5bc2b440e91e2aea0b88ea422beca42
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-07 12:08:31 -0700

    fix missing namespacing

--- tests/test-all.R
+++ tests/test-all.R
@@ -2,16 +2,16 @@ library('testthat')
 library('elastic')
 
 docs_bulk_tests <- function(x, index = NULL, type = NULL, chunk_size = 1000, raw=FALSE, ...) {
-  conn <- es_get_auth()
+  conn <- elastic::es_get_auth()
   url <- paste0(conn$base, ":", conn$port, '/_bulk')
-  tt <- POST(url, body = upload_file(x, type = "application/json"), ..., encode = "json")
+  tt <- httr::POST(url, body = httr::upload_file(x, type = "application/json"), ..., encode = "json")
   if (tt$status_code > 202) {
-    if (tt$status_code > 202) stop(content(tt)$error)
-    if (content(tt)$status == "ERROR" | content(tt)$status == 500) stop(content(tt)$error_message)
+    if (tt$status_code > 202) stop(httr::content(tt)$error)
+    if (httr::content(tt)$status == "ERROR" | httr::content(tt)$status == 500) stop(httr::content(tt)$error_message)
   }
-  res <- content(tt, as = "text")
+  res <- httr::content(tt, as = "text")
   res <- structure(res, class = "bulk_make")
-  if (raw) res else es_parse(res)
+  if (raw) res else elastic::es_parse(res)
 }
 
 invisible(elastic::connect())
