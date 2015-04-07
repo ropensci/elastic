2451f72fa9663cdf9106d00cd7cf6421ffe46758
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-07 12:20:51 -0700

    fix  namespacing one last time hopefully

--- tests/test-all.R
+++ tests/test-all.R
@@ -2,7 +2,7 @@ library('testthat')
 library('elastic')
 
 docs_bulk_tests <- function(x, index = NULL, type = NULL, chunk_size = 1000, raw=FALSE, ...) {
-  conn <- elastic::es_get_auth()
+  conn <- elastic:::es_get_auth()
   url <- paste0(conn$base, ":", conn$port, '/_bulk')
   tt <- httr::POST(url, body = httr::upload_file(x, type = "application/json"), ..., encode = "json")
   if (tt$status_code > 202) {
