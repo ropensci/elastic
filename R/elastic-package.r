8a2e293548467105e973e1693e5353b6ce592688
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-07 12:21:55 -0700

    fix #50 added notes about security within pkg and to readme

--- R/elastic-package.r
+++ R/elastic-package.r
@@ -39,6 +39,19 @@
 #'
 #' \code{Search(index="twitter")}
 #'
+#' @section Security:
+#' 
+#' Elasticsearch is insecure out of the box! If you are running Elasticsearch 
+#' locally on your own machine, no worries, but if you install on a server with 
+#' a public IP address, take the necessary precautions. There are a few options:
+#' 
+#' \itemize{ 
+#'  \item Shield \url{https://www.elastic.co/products/shield} - This is a paid
+#'  product - so probably only applicable to enterprise users
+#'  \item DIY security - there are a variety of techniques for securing your
+#'  Elasticsearch. I collected a number of resources in a blog post at
+#'  \url{http://recology.info/2015/02/secure-elasticsearch/}
+#' }
 #'
 #' @docType package
 #' @aliases elastic-package
