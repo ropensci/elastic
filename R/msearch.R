57122d0d1cc39a99cf57c2ebb0bd41c77c4adfab
Scott Chamberlain <myrmecocystus@gmail.com>
2016-01-05 12:14:21 -0800

    added msearch for multi-search, and tests, fix #103

---
+++ R/msearch.R
@@ -0,0 +1,33 @@
+#' @title Multi-search
+#' 
+#' @description Performs multiple searches, defined in a file
+#'
+#' @export
+#' @param x (character) A file path 
+#' @param raw (logical) Get raw JSON back or not.
+#' @param asdf (logical) If \code{TRUE}, use \code{\link[jsonlite]{fromJSON}} to parse JSON
+#' directly to a data.frame. If \code{FALSE} (Default), list output is given.
+#' @details This function behaves similarly to \code{\link{docs_bulk}} - performs
+#' searches based on queries defined in a file.
+#' @seealso \code{\link{Search_uri}} \code{\link{Search}}
+#' @examples \dontrun{
+#' connect()
+#' msearch1 <- system.file("examples", "msearch_eg1.json", package = "elastic")
+#' readLines(msearch1)
+#' msearch(msearch1)
+#' 
+#' cat('{"index" : "shakespeare"}', file = "~/mysearch.json", sep = "\n")
+#' cat('{"query" : {"match_all" : {}}, "from" : 0, "size" : 5}',  sep = "\n",
+#'    file = "~/mysearch.json", append = TRUE)
+#' msearch("~/mysearch.json")
+#' }
+msearch <- function(x, raw = FALSE, asdf = FALSE, ...) {
+  checkconn()
+  if (!file.exists(x)) stop("file ", x, " does not exist", call. = FALSE)
+  conn <- es_get_auth()
+  url <- paste0(conn$base, ":", conn$port, '/_msearch')
+  tt <- POST(url, make_up(), ..., body = upload_file(x, type = "application/json"), encode = "json")
+  geterror(tt)
+  res <- content(tt, as = "text")
+  if (raw) res else jsonlite::fromJSON(res, asdf)
+}
---
+++ tests/testthat/test-msearch.R
@@ -0,0 +1,49 @@
+context("msearch")
+
+invisible(connect())
+
+test_that("basic multi-search works", {
+
+  cat('{"index" : "shakespeare"}', file = "~/mysearch.json", sep = "\n")
+  cat('{"query" : {"match_all" : {}}, "from" : 0, "size" : 5}',  sep = "\n",
+      file = "~/mysearch.json", append = TRUE)
+  aa <- msearch("~/mysearch.json")
+  
+  expect_is(aa, "list")
+  expect_equal(length(aa), 1)
+  expect_equal(length(aa$responses), 1)
+  expect_is(aa$responses, "list")
+  
+  msearch1 <- system.file("examples", "msearch_eg1.json", package = "elastic")
+  bb <- msearch(msearch1)
+  
+  msearch2 <- system.file("examples", "msearch_eg2.json", package = "elastic")
+  cc <- msearch(msearch2)
+  
+  expect_is(bb, "list")
+  expect_equal(length(bb$responses), 1)
+  
+  expect_is(cc, "list")
+  expect_equal(length(cc$responses), 4)
+})
+
+test_that("multi-search fails well", {
+  
+  ## no index specified
+  ff <- tempfile(fileext = ".json")
+  cat('{"query" : {"match_all" : {}}, "from" : 0, "size" : 5}',  sep = "\n",
+      file = ff, append = TRUE)
+  expect_error(msearch(ff), "Validation Failed")
+  
+  ### same, but complete errors
+  invisible(connect(errors = "complete"))
+  expect_error(msearch(ff), "action_request_validation_exception")
+  
+  ## same as above
+  ff <- tempfile(fileext = ".json")
+  cat('{}',  sep = "\n", file = ff, append = TRUE)
+  expect_error(msearch(ff), "Validation Failed")
+  
+  ## file does not exist
+  expect_error(msearch("asdf"), "file asdf does not exist")
+})
