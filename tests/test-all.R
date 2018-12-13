16feb3ef063d83dd900250db5071d0aaf610cda6
Scott Chamberlain <myrmecocystus@gmail.com>
2018-12-13 11:30:33 -0800

    uncomment test-all lines and change most recent elasticsearch in travis file to 6.5.3

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,19 +1,19 @@
-# library('testthat')
-# library('elastic')
+library('testthat')
+library('elastic')
 
-# invisible(elastic::connect())
+invisible(elastic::connect())
 
-# if (elastic:::es_ver() < 600) {
-#   shakespeare <- system.file("examples", "shakespeare_data.json", package = "elastic")
-# } else {
-#   shakespeare <- system.file("examples", "shakespeare_data_.json", package = "elastic")
-# }
-# invisible(elastic::docs_bulk(shakespeare))
+if (elastic:::es_ver() < 600) {
+  shakespeare <- system.file("examples", "shakespeare_data.json", package = "elastic")
+} else {
+  shakespeare <- system.file("examples", "shakespeare_data_.json", package = "elastic")
+}
+invisible(elastic::docs_bulk(shakespeare))
 
-# plos <- system.file("examples", "plos_data.json", package = "elastic")
-# invisible(elastic::docs_bulk(plos))
+plos <- system.file("examples", "plos_data.json", package = "elastic")
+invisible(elastic::docs_bulk(plos))
 
-# omdb <- system.file("examples", "omdb.json", package = "elastic")
-# invisible(elastic::docs_bulk(omdb))
+omdb <- system.file("examples", "omdb.json", package = "elastic")
+invisible(elastic::docs_bulk(omdb))
 
-# test_check('elastic')
+test_check('elastic')
