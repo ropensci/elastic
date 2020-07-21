2864aee31bd18105fb678ff51f53d777eee1d9ad
Scott Chamberlain <myrmecocystus@gmail.com>
2020-07-21 08:53:24 -0700

    fix check for ES availability in tests

--- tests/test-all.R
+++ tests/test-all.R
@@ -3,19 +3,21 @@ library('elastic')
 
 x <- elastic::connect()
 try_conn <- tryCatch(x$ping(), error = function(e) e)
-if (inherits(try_conn, "error")) skip("Elasticsearch not available, skipping tests")
-
-if (x$es_ver() < 600) {
-  shakespeare <- system.file("examples", "shakespeare_data.json", package = "elastic")
+if (inherits(try_conn, "error")) {
+  cat("Elasticsearch not available, skipping tests")
 } else {
-  shakespeare <- system.file("examples", "shakespeare_data_.json", package = "elastic")
+  if (x$es_ver() < 600) {
+    shakespeare <- system.file("examples", "shakespeare_data.json", package = "elastic")
+  } else {
+    shakespeare <- system.file("examples", "shakespeare_data_.json", package = "elastic")
+  }
+  invisible(elastic::docs_bulk(x, shakespeare))
+
+  plos <- system.file("examples", "plos_data.json", package = "elastic")
+  invisible(elastic::docs_bulk(x, plos))
+
+  omdb <- system.file("examples", "omdb.json", package = "elastic")
+  invisible(elastic::docs_bulk(x, omdb))
+
+  test_check('elastic')  
 }
-invisible(elastic::docs_bulk(x, shakespeare))
-
-plos <- system.file("examples", "plos_data.json", package = "elastic")
-invisible(elastic::docs_bulk(x, plos))
-
-omdb <- system.file("examples", "omdb.json", package = "elastic")
-invisible(elastic::docs_bulk(x, omdb))
-
-test_check('elastic')
